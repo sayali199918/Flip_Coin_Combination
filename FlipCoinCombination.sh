fef0c8225975d282eecf104a4b5b3549c339f0cb
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-10 10:08:18 -0700

    [sayali] Added:Loop to store singlet combination in a dictionary determine percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -x
 echo "Welcome to flip Coin Combination Program"
-flipCoin=$(( RANDOM % 2 ))
-if [ $flipCoin -eq 1 ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+declare -A dict
+read -p "Enter the number of times you want to flip a coin:" n
+echo $n
+for (( i=0; i<$n; i++ ))
+do
+	flipCoin=$(( RANDOM % 2 ))
+	if [ $flipCoin -eq 1 ]
+	then
+		dict["H"]=$(( ${dict[H]} + 1 ))
+	else
+		dict["T"]=$(( ${dict[T]} + 1 ))
+	fi
+done
+echo ${dict[H]}  ${dict[T]}
+percentage=`echo ${dict[H]} $n | awk '{print ($1/$2)*1oo}'`
+echo "H:" $percentage"%"
+percentage=`echo ${dict[T]} $n | awk '{print ($1/$2)*1oo}'`
+echo "T:" $percentage"%"
+ 
